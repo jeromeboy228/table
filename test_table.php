f70bcafa9fa067bd1d7adb65dca8bd5e3ba15f9e
jeromeboy228 <egorbaranov77@gmail.com>
2020-04-26 22:58:22 +0200

    update

--- test_table.php
+++ test_table.php
@@ -19,14 +19,16 @@
             <td>Месяц поверки</td>
             <td>Прочее</td>
             <td>Статус поверки</td>
+            <td>Stat</td>
         </tr>
         <?php
-        $legth_table = 0;
+        $legth_table = 0; // длинна таблицы
+        $flag; // проверка состояния прибора (поверен или нет)
         $connect = mysqli_connect("localhost", "echo", "Jerome112", "test");
         if (!$connect) die("ошибка подключения к БД");
         $select = mysqli_select_db($connect, "test");
         if (!$select) die("ошибка выбора БД");
-        $qresult = mysqli_query($connect, "SELECT *  FROM Sheet1");
+        $qresult = mysqli_query($connect, "SELECT *  FROM List1");
         if (!$qresult) die("Ошибка с таблицей");
         while ($elem = mysqli_fetch_assoc($qresult)) {
             $legth_table++; ?>
@@ -38,19 +40,34 @@
                     <td><?php print("{$elem["D"]}") ?></td>
                     <td><?php print("{$elem["E"]}") ?></td>
                     <td><?php print("{$elem["F"]}") ?></td>
-                    <td><input type="checkbox" name="id<?php print("$legth_table"); ?>"> Поверенно(поз.<?php print("$legth_table"); ?>) </td>
-                </tr>
+                    <td><input type="checkbox" name="id<?php print("$legth_table"); ?>"> Поверенно(поз.<?php print("$legth_table"); ?>)
+                        <input type="hidden" name="check_<?php print("$legth_table"); ?>" value="1">
+                    </td>
+                    
 
-        <?php 
-        if(isset($_POST["id$legth_table"]) && $_POST["id$legth_table"])
-        echo $_POST["id$legth_table"];
-        else echo "problems";
-    }
-        ?>
+
+                <?php
+                if (isset($_POST["id$legth_table"]) && $_POST["id$legth_table"]) {
+                    $flag = $_POST["check_$legth_table"];
+                    printf("<td>$flag</td>");
+                    
+                } else {
+                    $flag = 0;
+                    printf("<td>$flag</td>");
+                    
+                }
+            }
+                ?>
+                </tr>
     </table>
-    <input type="submit"  value="lol" >
+    <input type="submit" value="lol">
     </form>
 
 </body>
 
 </html>
+<!-- 
+qresult=mysqli_query($connect,"INSERT  INTO table_work(type_dev,mark_dev,serial_num,mouth,exet)
+    values
+    ('".$type_dev."','".$mark_dev."','".$serial_num."','".$mounth."','".$exet."')
+    "); -->
