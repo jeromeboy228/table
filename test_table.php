cc845973966aa14edebf01867e84b42bb4756289
jeromeboy228 <egorbaranov77@gmail.com>
2020-05-01 23:44:14 +0200

    уже почти

--- test_table.php
+++ test_table.php
@@ -2,19 +2,22 @@
 <html lang="en">
 
 <head>
-<script src="https://ajax.googleapis.com/ajax/libs/jquery/1.12.4/jquery.min.js">
-</script>
+    <script src="https://ajax.googleapis.com/ajax/libs/jquery/1.12.4/jquery.min.js">
+    </script>
     <link rel="stylesheet" href="style.css">
     <meta charset="UTF-8">
     <meta name="viewport" content="width=device-width, initial-scale=1.0">
     <title>Document</title>
     <link rel="stylesheet" href="style.css">
-    <script src="demo.js"></script>
-
+    <!--     <script src="demo.js"></script>
+ -->
     <style type="text/css">
-    .color_table th, #first_tr { background-color: lightblue; }
-</style>
-    
+        .color_table th,
+        #first_tr {
+            background-color: lightblue;
+        }
+    </style>
+
 </head>
 
 <body>
@@ -28,6 +31,7 @@
             <td>Прочее</td>
             <td>Статус поверки</td>
             <td>Stat</td>
+            <td>button</td>
         </tr>
         <?php
         $legth_table = 0; // длинна таблицы
@@ -36,13 +40,15 @@
         if (!$connect) die("ошибка подключения к БД");
         $select = mysqli_select_db($connect, "test");
         if (!$select) die("ошибка выбора БД");
-        $qresult = mysqli_query($connect, "SELECT *  FROM Sheet1");
+        $qresult = mysqli_query($connect, "SELECT *  FROM List2");
         if (!$qresult) die("Ошибка с таблицей");
         while ($elem = mysqli_fetch_assoc($qresult)) {
             $legth_table++; ?>
             <form name="id<?php print("$legth_table"); ?>" action="test_table.php" method="post">
-                <tr <?php if ($elem["G"] == date("n")) print("class=\"new\"") ?>>
-                    <td><?php print("$legth_table") ?></td>
+                <tr <?php if ($elem["G"] == date("n")) print("class=\"new\"");
+                    if ($elem["check_"] == 1) print("class=\"for_pow\"");
+                    ?>>
+                    <td><?php print("{$elem["A"]}") ?></td>
                     <td><?php print("{$elem["B"]}") ?></td>
                     <td><?php print("{$elem["C"]}") ?></td>
                     <td><?php print("{$elem["D"]}") ?></td>
@@ -51,25 +57,35 @@
                     <td><input type="checkbox" name="id<?php print("$legth_table"); ?>"> Поверенно(поз.<?php print("$legth_table"); ?>)
                         <input type="hidden" name="check_<?php print("$legth_table"); ?>" value="1">
                     </td>
-                    
+                    <td> <input type="submit" value="lol">
+                    </td>
+
 
 
                 <?php
                 if (isset($_POST["id$legth_table"]) && $_POST["id$legth_table"]) {
                     $flag = $_POST["check_$legth_table"];
+                    $temp = mysqli_query($connect, "UPDATE List2 SET check_=1 WHERE A=$flag");
+                    if (!$temp) echo "bad =(";
                     printf("<td>$flag</td>");
-                    
-                    
                 } else {
                     $flag = 0;
                     printf("<td>$flag</td>");
-                    
                 }
             }
                 ?>
                 </tr>
     </table>
-    <input type="submit" value="lol">
+    <!--     <input type="submit" value="lol">
+ -->
+    <?php
+
+    /* $temp=mysqli_query($connect,"UPDATE List2 SET check_=1 WHERE A=3");
+if(!$temp) die("Проблемы отправки формы"); 
+    echo "its all good,check DB";*/
+
+    ?>
+
     </form>
 
 </body>
